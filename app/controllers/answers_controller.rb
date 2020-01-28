53f702aa4a0271e20702968e46e63f3c6b45faa1
Sachin Singh <sachin.y87@gmail.com>
2020-01-29 02:14:02 +0530

    unused actions removed

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,25 +1,10 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
-  end
-
   # GET /answers/new
   def new
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(user_id: current_user.id)
-  end
-
-  # GET /answers/1/edit
-  def edit
   end
 
   # POST /answers
@@ -37,30 +22,6 @@ class AnswersController < ApplicationController
         format.html { render :new }
         format.json { render json: @answer.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
-  def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /answers/1
-  # DELETE /answers/1.json
-  def destroy
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,10 +15,6 @@ class QuestionsController < ApplicationController
     @question = current_user.questions.build
   end
 
-  # GET /questions/1/edit
-  def edit
-  end
-
   # POST /questions
   # POST /questions.json
   def create
@@ -32,30 +28,6 @@ class QuestionsController < ApplicationController
         format.html { render :new }
         format.json { render json: @question.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
-  def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /questions/1
-  # DELETE /questions/1.json
-  def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
